1c48e3d94a0507098749943a632813dcae51f9e5
shinmiy <shinmiyaccs@gmail.com>
2020-10-14 19:04:27 +0900

    tweak vitamins_included layout

--- keyboards/vitamins_included/keymaps/shinmiy/keymap.c
+++ keyboards/vitamins_included/keymaps/shinmiy/keymap.c
@@ -108,8 +108,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT_ortho_4x12( \
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC, \
-  _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    JA_AT,      JA_LBRC, JA_RBRC, JA_ENVL, \
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_MHEN, KC_HENK, JA_CLON, \
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    JA_AT,      JA_LBRC, JA_RBRC, JA_ENVL, \
+  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_LANG2,KC_LANG1,JA_CLON, \
   _______, _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY \
 ),
 
@@ -127,7 +127,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_ortho_4x12( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, JA_HAT,  JA_AT,   KC_RBRC, KC_BSLS, \
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_MHEN, KC_HENK, _______, \
+  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_LANG2,KC_LANG1,_______, \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
 ),
 
@@ -162,8 +162,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ARROW] = LAYOUT_ortho_4x12( \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  _______, KC_LEFT, KC_UP  , KC_DOWN,  KC_RGHT, _______, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______, _______, \
-  _______, KC_HENK, KC_MHEN, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______, _______, KC_LEFT, KC_UP  , KC_DOWN, KC_RGHT, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______, _______, \
+  _______, KC_LANG1,KC_LANG2,_______, _______, _______, _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
 ),
 
@@ -171,9 +171,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * .-----------------------------------------.-----------------------------------------.
  * | Bksp |   P  |   O  |   I  |   U  |   Y  |      |  7   |  8   |  9   |      |      |
  * +------+------+------+------+------+------|------+------+------+------+------+-------
- * |  '   |   ;  |   L  |   K  |   J  |   H  |      |  4   |  5   |  6   |      |      |
+ * |Enter |   ;  |   L  |   K  |   J  |   H  |      |  4   |  5   |  6   |      |      |
  * +------+------+------+------+------+------|------+------+------+------+------+------|
- * |Enter |   /  |   .  |   ,  |   M  |   N  |      |  1   |  2   |  3   |      |      |
+ * | _    |   /  |   .  |   ,  |   M  |   N  |      |  1   |  2   |  3   |      |      |
  * +------+------+------+------+------+------|------+------+------+------+------+------|
  * |Space |      |      |      |      |      |      |  0   |      |      |      |      |
  * '-----------------------------------------'-----------------------------------------'
